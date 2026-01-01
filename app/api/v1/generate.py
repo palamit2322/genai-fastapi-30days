9b44495a4866a9c7ca635b57c2d90fb4d36d9c8e
Amit Pal <amitpal@MacBookPro.lan>
2026-01-01 17:47:00 +0530

    Implement Exception handling

--- app/api/v1/generate.py
+++ app/api/v1/generate.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends, HTTPException, status
 from app.models.prompt import PromptRequest
 from app.services.llm_Service import LLMService
 from app.core.dependencies import get_llm_service
@@ -8,6 +8,12 @@ router=APIRouter()
 def generate_text(request:PromptRequest, llm_service:LLMService=Depends(get_llm_service)):
     
     result=llm_service.generate(request.prompt)
+
+    if not request.prompt.strip():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="prompt cannot be empty"
+        )
     return {
         "response": result
     }
