68ae26c14f26b53841c34181e117b895dae56992
Amit Pal <amitpal@MacBookPro.lan>
2026-01-02 19:25:07 +0530

    Day 7: Environment based config management

---
+++ app/core/config.py
@@ -0,0 +1,17 @@
+from  pydantic_settings import BaseSettings
+from functools import lru_cache
+import os
+
+class Settings(BaseSettings):
+    ENV: str="dev"
+    OPENAI_API_KEY: str
+    MODEL_NAME: str
+    TEMPERATURE: float=0.7
+
+    class Config():
+        env_file=f".env.{os.getenv('dev','prod')}"
+        case_sensitive=True
+
+@lru_cache
+def get_settings():
+    return Settings()
--- app/services/llm_Service.py
+++ app/services/llm_Service.py
@@ -1,6 +1,15 @@
 import asyncio
+from app.core.config import get_settings
 class LLMService:
+
+    def __init__(self):
+        self.settings=get_settings()
+
     async def generate(self,prompt:str)->str:
         asyncio.sleep(5)
-        return f"llm response for: {prompt}"
+        return (
+            f"Model: {self.settings.MODEL_NAME}",
+            f"Temperature: {self.settings.TEMPERATURE}",
+            f"Prompt: {prompt}"
+        )
     
