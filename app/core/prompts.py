660f9cc5caef028ac540376a604e0958963fce62
Amit Pal <amitpal@MacBookPro.lan>
2026-01-03 18:38:31 +0530

    Impelment the concept of prompts

---
+++ app/core/prompts.py
@@ -0,0 +1,5 @@
+SYSTEM_PROMPT = """
+You are a helpful GenAI assistant.
+Explain things in very simple language.
+Avoid technical jargon unless asked.
+"""
--- app/services/llm_Service.py
+++ app/services/llm_Service.py
@@ -1,18 +1,20 @@
 import asyncio
 from  openai import OpenAI
 from app.core.config import get_settings
+from app.core.prompts import SYSTEM_PROMPT
 class LLMService:
 
     def __init__(self):
         self.settings=get_settings()
         self.client=OpenAI(api_key=self.settings.OPENAI_API_KEY)
 
-    async def generate(self,prompt:str)->str:
+    async def generate(self,user_prompt:str)->str:
         #asyncio.sleep(5)
         response=self.client.chat.completions.create(
             model=self.settings.MODEL_NAME,
             messages=[
-                {"role":"user","content":prompt}
+                {"role":"system","content":SYSTEM_PROMPT},
+                {"role":"user","content":user_prompt}
             ],
             temperature=self.settings.TEMPERATURE
 
