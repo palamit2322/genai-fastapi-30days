237e34b8ec8cbc2227857fdef0c5dd606d89c57d
Amit Pal <amitpal@MacBookPro.lan>
2026-01-03 18:24:13 +0530

    implement the openAI Chat Model

--- app/services/llm_Service.py
+++ app/services/llm_Service.py
@@ -1,15 +1,23 @@
 import asyncio
+from  openai import OpenAI
 from app.core.config import get_settings
 class LLMService:
 
     def __init__(self):
         self.settings=get_settings()
+        self.client=OpenAI(api_key=self.settings.OPENAI_API_KEY)
 
     async def generate(self,prompt:str)->str:
-        asyncio.sleep(5)
+        #asyncio.sleep(5)
+        response=self.client.chat.completions.create(
+            model=self.settings.MODEL_NAME,
+            messages=[
+                {"role":"user","content":prompt}
+            ],
+            temperature=self.settings.TEMPERATURE
+
+        )
         return (
-            f"Model: {self.settings.MODEL_NAME}",
-            f"Temperature: {self.settings.TEMPERATURE}",
-            f"Prompt: {prompt}"
+            response.choices[0].message.content
         )
     
